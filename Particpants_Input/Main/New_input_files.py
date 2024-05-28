a17f57c0b60b384582d97fcdd8713d0561e26e33
Daniela Gallegos Dupuis <danigallegdup@gmail.com>
2024-05-28 11:57:31 -0700

    start get it all together

--- Particpants_Input/Main/New_input_files.py
+++ Particpants_Input/Main/New_input_files.py
@@ -1,6 +1,6 @@
 import subprocess
 
-scripts = ["Step0_table_csv.py", ]
+scripts = ["Step0_table_csv.py","Step2_make_24_permuations.py", "step3_extend.py", "step4.py", "step5.py", "step6.py", "step7.py", "step8.py", "step9.py"]
 
 # Call each script
 for script in scripts:
